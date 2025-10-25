9a1475b406ce0ba2ce53421783f5f636ef6bc8d6
Axonide <alvusebastian@gmail.com>
2025-10-25 11:09:07 +0700

    up

---
+++ Run ESB Auto Reporting.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting ESB Auto Reporting Script...
+echo.
+cd /d "%USERPROFILE%\Desktop\code\esb-periodic-webscrap-reporting-script"
+python "ESB Auto Reporting.py"
+echo.
+echo Script execution completed.
+pause
---
+++ Run ESB Instant Report.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting ESB Instant Report Script...
+echo.
+cd /d "%USERPROFILE%\Desktop\code\esb-periodic-webscrap-reporting-script"
+python "ESB Instant Report_now.py"
+echo.
+echo Script execution completed.
+pause
